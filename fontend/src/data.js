d53600818ea15f5cc6415f3c5b4ffdfd2a592671
sivaprakas_ng <siva49uj630t@gmail.com>
2021-02-12 10:35:23 +0800

    first commit

---
+++ fontend/src/data.js
@@ -0,0 +1,72 @@
+const data= {
+    products : [
+        {
+            _id: 1,
+            name: "Nike silm shirts",
+            category: "shirts",
+            image: "./images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality products"
+        },
+        {
+            _id: 2,
+            name: "Camelactive shirts",
+            category: "shirts",
+            image: "./images/p2.jpg",
+            price: 70,
+            brand: "Camel Active",
+            rating: 4.0,
+            numReviews: 30,
+            description: "high quality products"
+        },
+        {
+            _id: 3,
+            name: "Forest shirts",
+            category: "shirts",
+            image: "./images/p3.jpg",
+            price: 45,
+            brand: "Forest",
+            rating: 3.0,
+            numReviews: 45,
+            description: "high quality products"
+        },
+        {
+            _id: 4,
+            name: "Kickers pants",
+            category: "pants",
+            image: "./images/p4.jpg",
+            price: 67,
+            brand: "Kickers",
+            rating: 4.0,
+            numReviews: 10,
+            description: "high quality products"
+        },
+        {
+            _id: 5,
+            name: "Dockers pants",
+            category: "pants",
+            image: "./images/p5.jpg",
+            price: 90,
+            brand: "Dockers",
+            rating: 5,
+            numReviews: 35,
+            description: "high quality products"
+        },
+        {
+            _id: 6,
+            name: "Soda pants",
+            category: "pants",
+            image: "./images/p6.jpg",
+            price: 25,
+            brand: "Soda",
+            rating: 2.5,
+            numReviews: 60,
+            description: "high quality products"
+        }
+    ]
+};
+
+export default data;
